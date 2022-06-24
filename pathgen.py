101282a38482e7b5911f7c24f59c2bb127b23695
William-Sinn <sinnwilliam7@gmail.com>
2022-06-23 20:31:50 -0400

    Fixed some issues with the hamiltonian cycles

--- pathgen.py
+++ pathgen.py
@@ -1,5 +1,6 @@
 import random
-
+from test import *
+from deepdiff import DeepDiff
 
 def prim_maze_gen(n):
     dirs = {}
@@ -20,7 +21,6 @@ def prim_maze_gen(n):
     adj_points = set()
 
     while len(visited) != vert:
-        # Stores the position of the current cell in the grid
         x_cord = curr_point[0]
         y_cord = curr_point[1]
 
@@ -63,19 +63,13 @@ def ham_cycle_gen(dirs, n):
     for i in range(n):
         for j in range(n):
             if j > 0:
-                if j == n - 1:
-                    ham_cycle[j * 2 + 1, i * 2] = [(j * 2 + 1, i * 2 + 1)]
                 if 'right' not in dirs[j - 1, i]:
-                    ham_cycle[j * 2 + 1, i * 2] = [(j * 2 + 1, i * 2 + 1)]
                     if (j * 2, i * 2) in ham_cycle:
                         ham_cycle[j * 2, i * 2] += [(j * 2, i * 2 + 1)]
                     else:
                         ham_cycle[j * 2, i * 2] = [(j * 2, i * 2 + 1)]
 
             if j < n - 1:
-                if j == 0:
-                    ham_cycle[j * 2, i * 2] = [(j * 2, i * 2 + 1)]
-                ham_cycle[j * 2, i * 2] = [(j * 2 + 1, i * 2)]
                 if 'right' in dirs[j, i]:
                     ham_cycle[j * 2 + 1, i * 2] = [(j * 2 + 2, i * 2)]
                     ham_cycle[j * 2 + 1, i * 2 + 1] = [(j * 2 + 2, i * 2 + 1)]
@@ -83,16 +77,12 @@ def ham_cycle_gen(dirs, n):
                     ham_cycle[j * 2 + 1, i * 2] = [(j * 2 + 1, i * 2 + 1)]
 
             if i > 0:
-                if i == n - 1:
-                    ham_cycle[j * 2, i * 2 + 1] = [(j * 2 + 1, i * 2 + 1)]
                 if 'down' not in dirs[j, i - 1]:
                     ham_cycle[j * 2, i * 2] = [(j * 2 + 1, i * 2)]
                 if ham_cycle == 0:
                     ham_cycle[j * 2, i * 2] = [(j * 2 + 1, i * 2)]
 
             if i < n - 1:
-                if i == 0:
-                    ham_cycle[j * 2, i * 2] = [(j * 2 + 1, i * 2)]
                 if 'down' in dirs[j, i]:
                     ham_cycle[j * 2, i * 2 + 1] = [(j * 2, i * 2 + 2)]
                     if (j * 2 + 1, i * 2 + 1) in ham_cycle:
@@ -101,6 +91,24 @@ def ham_cycle_gen(dirs, n):
                         ham_cycle[j * 2 + 1, i * 2 + 1] = [(j * 2 + 1, i * 2 + 2)]
                 else:
                     ham_cycle[j * 2, i * 2 + 1] = [(j * 2 + 1, i * 2 + 1)]
+
+            if j == n - 1:
+                ham_cycle[j * 2 + 1, i * 2] = [(j * 2 + 1, i * 2 + 1)]
+            if j == 0:
+                ham_cycle[j * 2, i * 2] = [(j * 2, i * 2 + 1)]
+            if i == 0:
+                ham_cycle[j * 2, i * 2] = [(j * 2 + 1, i * 2)]
+            if i == n - 1:
+                ham_cycle[j * 2, i * 2 + 1] = [(j * 2 + 1, i * 2 + 1)]
+            if j == 0 and i == 0:
+                ham_cycle[j * 2, i * 2] = [(j * 2 + 1, i * 2)]
+                ham_cycle[j * 2, i * 2] += [(j * 2, i * 2 + 1)]
+            if j == n - 1 and i == 0:
+                ham_cycle[j * 2, i * 2] = [(j * 2 + 1, i * 2)]
+                ham_cycle[j * 2 + 1, i * 2] = [(j * 2 + 1, i * 2 + 1)]
+            if j == 0 and i == n - 1:
+                ham_cycle[j * 2, i * 2] = [(j * 2, i * 2 + 1)]
+                ham_cycle[j * 2, i * 2 + 1] = [(j * 2 + 1, i * 2 + 1)]
 
     return ham_cycle
 
@@ -112,33 +120,54 @@ def matrix_conv(cycle, n):
     size = n ** 2
 
     while len(path) != size:
-
         if prev in cycle and (prev[0] + 1, prev[1]) in cycle[prev] and prev_dir != 'left':
             prev = (prev[0] + 1, prev[1])
             path.append(prev)
             prev_dir = 'right'
+
         elif prev in cycle and (prev[0], prev[1] + 1) in cycle[prev] and prev_dir != 'up':
             prev = (prev[0], prev[1] + 1)
             path.append(prev)
             prev_dir = 'down'
+
         elif (prev[0] - 1, prev[1]) in cycle and prev in cycle[prev[0] - 1, prev[1]] and prev_dir != 'right':
             prev = (prev[0] - 1, prev[1])
             path.append(prev)
             prev_dir = 'left'
+
         else:
             prev = (prev[0], prev[1] - 1)
             path.append(prev)
             prev_dir = 'up'
 
-    return path
+    matrix_path = []
+    array = []
+    for x in range(n):
+        for y in range(n):
+            array.append(0)
+        matrix_path.append(array)
+        array = []
+
+    i = 0
+    for cords in path:
+        matrix_path[cords[1]][cords[0]] = i
+        i += 1
+    return matrix_path
 
 
-thing = prim_maze_gen(2)
-test = ham_cycle_gen(thing, 2)
-# last = path_generator(test, 2 * 2 * 4)
-print(len(test))
+pling = prim_maze_gen(3)
+pest = ham_cycle_gen(pling, 3)
+print(pest)
+ruff = matrix_conv(pest, 6)
+# print("prim maze", ruff)
+thing = prim_maze_gen(3)
+test = hamiltonian_cycle(3, 3, pling)
 print(test)
-stuff = matrix_conv(test, 4)
-print(stuff)
-# print(last)
-# print(len(last))
+print(DeepDiff(pest, test))
+# test = ham_cycle_gen(thing, 3)
+stuff = matrix_conv(test, 6)
+# print("my maze", stuff)
+maze = prim_maze_generator(3, 3)
+ham = hamiltonian_cycle(3, 3, maze)
+result = path_generator(ham, 3 * 3 * 4)
+# print("Proper result", result)
